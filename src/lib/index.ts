5a2a472a607152c44e6c19ac9e0642148859b05f
iGameCreep <gamecreepbs@gmail.com>
2023-09-21 19:07:56 +0200

    Added exports

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -9,4 +9,15 @@ export * from './apis/core/dtos/api-dto';
 export * from './apis/core/dtos/paginated';
 export * from './apis/core/components/requests/crud-http-client';
 export * from './apis/core/components/lists/ListComponent';
-export * from './apis/core/components/query.builder';
+export * from './apis/core/components/search.builder';
+
+/**
+ * FunixGaming API
+ */
+export * from './apis/funixgaming-api/funixbot/dtos/funixbot-automessage-dto';
+export * from './apis/funixgaming-api/funixbot/dtos/funixbot-command-dto';
+export * from './apis/funixgaming-api/funixbot/dtos/funixbot-user-exp-dto';
+export * from './apis/funixgaming-api/funixbot/enums/funixbot-command-type';
+export * from './apis/funixgaming-api/funixbot/services/funixbot-automessages-crud-service';
+export * from './apis/funixgaming-api/funixbot/services/funixbot-commands-crud-service';
+export * from './apis/funixgaming-api/funixbot/services/funixbot-user-exp-crud-service';
