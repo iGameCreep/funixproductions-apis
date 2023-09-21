366b6d20a5ccf4a1cb4d6df1890d7dd6bfcd04be
iGameCreep <gamecreepbs@gmail.com>
2023-09-21 19:06:45 +0200

    Fixed paginated

--- src/lib/apis/core/dtos/paginated.ts
+++ src/lib/apis/core/dtos/paginated.ts
@@ -1,4 +1,3 @@
-import { QueryBuilder } from "../components/query.builder";
 import {ApiDTO} from "./api-dto";
 
 export class Paginated<T extends ApiDTO> {
@@ -9,9 +8,21 @@ export class Paginated<T extends ApiDTO> {
   totalElementsDatabase: number = 0;
 }
 
+/**
+ * Represents a configuration object for managing page options.
+ */
 export class PageOption {
-  elemsPerPage: number = 15;
-  page: number = 0;
-  sort: string = '';
-  search?: QueryBuilder;
+  /**
+   * Constructs a new PageOption object with optional initial values.
+   * @param {number} [elemsPerPage=15] - The number of elements to display per page (default: 15).
+   * @param {number} [page=0] - The current page number (default: 0).
+   * @param {string} [sort=''] - The sorting criteria (default: '').
+   * @param {string} [search=''] - The search query string (default: '').
+   */
+  constructor(
+    public elemsPerPage: number = 15,
+    public page: number = 0,
+    public sort: string = '',
+    public search: string = ''
+  ) {}
 }
